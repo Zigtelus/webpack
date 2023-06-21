8d1fc8b09083813e6356a36ec2e6e7d5e53d3da8
Zigtelus <Samuray911@mail.ru>
2023-06-22 03:27:00 +0600

    add commits for config of webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './index.less'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import './index.less';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 // @ts-ignore
-import App from './components/app.tsx'
-import { Provider } from 'react-redux'
+import App from './components/app.tsx';
+import { Provider } from 'react-redux';
 // @ts-ignore
-import store from './redux/store.ts'
+import store from './redux/store.ts';
 
 render(
   <BrowserRouter>
